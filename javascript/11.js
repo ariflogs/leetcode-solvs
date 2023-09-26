7a347ebdab778410e6274d7fb6f0adcc61301eb0
ariflogs <devarifhossain@gmail.com>
2023-09-26 09:55:38 +0600

    Initial commit with solutioin of (Container With Most Water)

---
+++ javascript/11.js
@@ -0,0 +1,38 @@
+/**
+ * Problem: 11. Container With Most Water
+ * Reference: https://leetcode.com/problems/container-with-most-water/
+ */
+
+/**
+ * 
+ * @param {number[]} height 
+ * @returns {number}
+ */
+
+// Solution 1 
+    // -> Time Complexity: O(n)
+    // -> Space Complexity: O(n)
+
+var maxArea = function(height) {
+    let maxHeight = 0;
+    let firstPointer = 0;
+    let lastPointer = height.length - 1;
+
+    for(let i = 0; i < height.length; i++) { 
+        const areaWeight = lastPointer - firstPointer;
+        const areaHeight = Math.min(height[firstPointer], height[lastPointer])
+        const area = areaHeight * areaWeight;
+
+        if (area > maxHeight) {
+            maxHeight = area;
+        }
+
+        if (height[firstPointer] > height[lastPointer]) {
+            lastPointer--
+        } else {
+            firstPointer++
+        }
+    }
+
+    return maxHeight;
+};
