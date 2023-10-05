7048bfbf4b33e85b916a4469d97a04e04a9a94f9
ariflogs <devarifhossain@gmail.com>
2023-10-05 12:12:19 +0600

    added solution 2 for problem 844

--- javascript/844_Backspace_String_Compare.js
+++ javascript/844_Backspace_String_Compare.js
@@ -15,6 +15,7 @@
 
 var filterChar = function (str, char) {
     let filteredStr = "";
+    let
 
     for (i = 0; i < str.length; i++) {
         if (str[i] !== char) {
@@ -32,4 +33,54 @@ var backspaceCompare = function (s, t) {
     const filteredT = filterChar(t, "#")
 
     return filteredS === filteredT
+};
+
+// Solution 2
+// -> Time Complexity: O(n)
+// -> Space Complexity: O(1)
+
+var backspaceCompare = function (s, t) {
+    let sPointer = s.length - 1;
+    let tPointer = t.length - 1;
+
+    let matched = true;
+    let i = 0;
+    while (i < s.length || i < t.length) {
+        if (sPointer < 0 && tPointer < 0) {
+            break;
+        }
+        if (s[sPointer] === "#") {
+            let back = 2;
+            while (back > 0) {
+                sPointer--;
+                back--;
+
+                if (s[sPointer] === "#") {
+                    back += 2;
+                }
+            }
+        }
+
+        if (t[tPointer] === "#") {
+            let back = 2;
+            while (back > 0) {
+                tPointer--;
+                back--;
+
+                if (t[tPointer] === "#") {
+                    back += 2;
+                }
+            }
+        }
+
+        if (s[sPointer] !== t[tPointer]) {
+            matched = false;
+            break;
+        }
+
+        sPointer--;
+        tPointer--;
+    }
+
+    return matched;
 };
